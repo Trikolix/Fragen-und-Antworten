9f48124907fd882c660091414dd33d0f01f80e12
Trikolix <ch_helbig@mail.de>
2018-03-01 15:23:27 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+  <title>Fragen & Antworten</title>              
+	<link href="style.css" type="text/css" rel="stylesheet">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="Fragen und Antwprten, Der Dozent stellt Fragen, die Studenten antworten." />
+	<meta name="keywords" content="Fragen, Antworten, Quiz, Auswertung, BA-Glauchau" />
+	<meta name="language" content="de, at" />
+  </head>  
+  <body>
+  <center> 
+  <div id="header">
+	Fragen und Antworten
+  </div>
+  <div id="main">
+  <form>
+    <p>Wie schmeckt Bier am besten?</p>
+    <fieldset>
+		<input type="radio" id="answer_a" name="Frage" value="Mastercard"><label for="answer_a">In rauen Mengen (>5Liter)</label>
+		<input type="radio" id="answer_b" name="Frage" value="Mastercard"><label for="answer_b">Ich mag kein Bier.</label>
+		<input type="radio" id="answer_c" name="Frage" value="Mastercard"><label for="answer_c">Kalt</label>
+		<input type="radio" id="answer_d" name="Frage" value="Mastercard"><label for="answer_c">Saufen! Morgens, Mittags, Abends ich will saufen!</label>
+    </fieldset>
+  </form>
 <?php
 
+
 ?>
+  </div>
+  </center>
+</body>
