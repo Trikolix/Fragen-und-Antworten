792584e7e4118e6bbd94dc84a03c868b82778cbc
Trikolix <ch_helbig@mail.de>
2018-05-15 14:06:27 +0200

    Add files via upload

--- project_x.sql
+++ project_x.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 26. Apr 2018 um 13:12
+-- Erstellungszeit: 15. Mai 2018 um 14:05
 -- Server-Version: 10.1.31-MariaDB
 -- PHP-Version: 7.2.4
 
@@ -19,65 +19,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `project_x`
+-- Datenbank: `martin`
 --
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `answers`
---
-
-CREATE TABLE `answers` (
-  `id` int(11) NOT NULL,
-  `question_id` int(11) NOT NULL,
-  `answer` varchar(127) NOT NULL,
-  `flag` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `answers`
---
-
-INSERT INTO `answers` (`id`, `question_id`, `answer`, `flag`) VALUES
-(1, 1, 'Ja!', 1),
-(2, 1, 'Definitiv!', 1),
-(3, 1, 'Vielleicht', 0),
-(4, 1, 'Aber sicher doch!', 1);
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `given_answers`
---
-
-CREATE TABLE `given_answers` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `question_id` int(11) NOT NULL,
-  `answer_id` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `flag` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `questions`
---
-
-CREATE TABLE `questions` (
-  `id` int(11) NOT NULL,
-  `question` text NOT NULL,
-  `creator_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `questions`
---
-
-INSERT INTO `questions` (`id`, `question`, `creator_id`) VALUES
-(1, 'Wird dieser Beleg mit einer 1,0 bewertet werden?', 1);
 
 -- --------------------------------------------------------
 
@@ -88,64 +31,15 @@ INSERT INTO `questions` (`id`, `question`, `creator_id`) VALUES
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `real_name` varchar(50) NOT NULL,
   `user_group_id` int(11) NOT NULL,
   `registrationdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Daten für Tabelle `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `real_name`, `user_group_id`, `registrationdate`) VALUES
-(1, 'Superadmin', 'Passwort', 'Achim der Große', 1, '2018-04-26 11:11:17'),
-(2, 'Trikolix', '123456', 'Christian', 2, '2018-04-26 11:11:17'),
-(3, 'Martinwurst', 'golf', 'Martin', 2, '2018-04-26 11:11:17'),
-(4, 'Camtheone', 'pullermann', 'René', 2, '2018-04-26 11:11:17'),
-(5, 'DJ Penzel', 'Beleg', 'Penzel', 3, '2018-04-26 11:11:58');
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `user_group`
---
-
-CREATE TABLE `user_group` (
-  `id` int(11) NOT NULL,
-  `bezeichnung` varchar(127) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Daten für Tabelle `user_group`
---
-
-INSERT INTO `user_group` (`id`, `bezeichnung`) VALUES
-(1, 'Superadmin'),
-(2, 'Admin'),
-(3, 'Studenten');
-
---
 -- Indizes der exportierten Tabellen
 --
-
---
--- Indizes für die Tabelle `answers`
---
-ALTER TABLE `answers`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indizes für die Tabelle `given_answers`
---
-ALTER TABLE `given_answers`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indizes für die Tabelle `questions`
---
-ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indizes für die Tabelle `users`
@@ -155,44 +49,14 @@ ALTER TABLE `users`
   ADD KEY `user_group_id` (`user_group_id`);
 
 --
--- Indizes für die Tabelle `user_group`
---
-ALTER TABLE `user_group`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT für exportierte Tabellen
 --
-
---
--- AUTO_INCREMENT für Tabelle `answers`
---
-ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT für Tabelle `given_answers`
---
-ALTER TABLE `given_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `questions`
---
-ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT für Tabelle `user_group`
---
-ALTER TABLE `user_group`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints der exportierten Tabellen
