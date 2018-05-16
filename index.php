06943080255d4fc8e9eca5980405e76e6befad44
René Tirschmann <rene.tirschmann@haus-der-edv.de>
2018-05-16 18:58:28 +0200

    Fragen holen verbessert
    - es werden nur noch Fragen geholt und angezeigt, die vom Nutzer noch nicht beantwortet wurden
    - wenn bei einer Frage keine Antworten gefunden werden, wird eine Meldung diesbezüglich angezeigt
    - sind alle Fragen von einem Nutzer beantwortet wurden bekommt dieser eine Meldung darüber
    - alggemeine verbesserung im Codestyle der Index-Datei und ergänzung einiger Überprüfungen

--- index.php
+++ index.php
@@ -2,37 +2,38 @@
 include "connect.php";
 if (isset($_GET['answered']))
 {
-	$answer = $_POST['Frage'];
+	
+	
 	if (isset($_SESSION['userid']))
 	{
-		$statement = mysqli_query($db, "SELECT * FROM answers WHERE id=".$answer."");
-		$row = mysqli_fetch_object($statement);
-		$question = $row->question_id;
-		$flag = $row->flag;
-		
-		$count = mysqli_num_rows(mysqli_query($db, "SELECT * FROM given_answers WHERE user_id=".$_SESSION['userid']." AND question_id=".$question.""));
-		if ($count > 0)
-		{
-			//Eintrag in Datenbank bereits vorhanden - ggf. Antwort korrigieren, bis jetzt nur Ausgabe Fehlermeldung
-			$error = "Eintrag in Datenbank nicht möglich. Es ist bereits ein Eintrag für die Frage von dem User vorhanden.";
-		}
-		else
-		{
-			//Antwort wird in Datenbank geschrieben
-			$statement = mysqli_query($db, "INSERT INTO given_answers (user_id, question_id, answer_id, flag) VALUES (".$_SESSION['userid'].", ".$question.", ".$answer.", ".$flag.")");
-			if ($flag == 1)
+		if (isset($_POST['Frage'])){
+			$answer = $_POST['Frage'];
+			$statement = mysqli_query($db, "SELECT * FROM answers WHERE id=".$answer."");
+			$row = mysqli_fetch_object($statement);
+			$question = $row->question_id;
+			$flag = $row->flag;
+			
+			$count = mysqli_num_rows(mysqli_query($db, "SELECT * FROM given_answers WHERE user_id=".$_SESSION['userid']." AND question_id=".$question.""));
+			if ($count > 0)
 			{
-				echo "<script type='text/javascript'>alert('Deine Antwort ist richtig!')</script>";
+				//Eintrag in Datenbank bereits vorhanden - ggf. Antwort korrigieren, bis jetzt nur Ausgabe Fehlermeldung
+				$error = "Eintrag in Datenbank nicht möglich. Es ist bereits ein Eintrag für die Frage von dem User vorhanden.";
 			}
-			else 
+			else
 			{
-				echo "<script type='text/javascript'>alert('Deine Antwort ist falsch!')</script>";
+				//Antwort wird in Datenbank geschrieben
+				$statement = mysqli_query($db, "INSERT INTO given_answers (user_id, question_id, answer_id, flag) VALUES (".$_SESSION['userid'].", ".$question.", ".$answer.", ".$flag.")");
+				if ($flag == 1)
+				{
+					echo "<script type='text/javascript'>alert('Deine Antwort ist richtig!')</script>";
+				}
+				else 
+				{
+					echo "<script type='text/javascript'>alert('Deine Antwort ist falsch!')</script>";
+				}
 			}
 		}
-		
-	}
-	else
-	{
+	} else {
 		$error = "Melden Sie sich bitte erst an um eine Frage beantworten zu können. <a href='login.php'>Hier gehts zum Login.</a><br>";
 	}
 }
@@ -48,20 +49,6 @@ if (isset($_GET['answered']))
 	<meta name="language" content="de, at" />
   </head>  
   <body>
-  <div id="login">
-	<?php
-		if (isset($_SESSION['userid']))
-		{
-			$statement = mysqli_query($db, "SELECT * FROM users WHERE id=".$_SESSION['userid']."");
-			$row = mysqli_fetch_object($statement);
-			echo "Sie sind eingeloggt als ".$row->username.". | <a href='submit_question.php'>Frage einreichen</a> | <a href='logout.php'>Ausloggen</a>";
-		}
-		else
-		{
-			echo "<a href='login.php'>Einloggen</a>";
-		}	
-	?>
-  </div>
   <center> 
   <div id="header">
 	Fragen und Antworten
@@ -69,24 +56,46 @@ if (isset($_GET['answered']))
   <div id="main">
   <?php
 	
-	if (isset($error))
+	if (isset($error)){
 		echo "<center><div id='error'>".$error."</div></center>";
-		
-	$statement = mysqli_query($db, "SELECT * FROM questions ORDER BY RAND() LIMIT 1");
-	while ($row = mysqli_fetch_object($statement))
-	{
-		$id = $row->id;
-		echo "<form action='?answered=1' method='post'>
-				<p>".$row->question."</p>
-				<fieldset>";
 	}
-	$statement = mysqli_query($db, "SELECT * FROM answers WHERE question_id=".$id." ORDER BY RAND()");
-	while ($row = mysqli_fetch_object($statement))
-	{
-		echo "<input type='radio' id='answer_a' name='Frage' value='".$row->id."'><label for='answer_a'>".$row->answer."</label>";
+	
+	$id = '';	
+	$get_question = mysqli_query($db, "SELECT * 
+										FROM `questions` 
+										WHERE id NOT IN (
+											SELECT question_id
+											FROM `given_answers` 
+											WHERE user_id = ".$_SESSION['userid'].") 
+										ORDER BY RAND() LIMIT 1");
+	$question_count = mysqli_num_rows($get_question);
+	if($question_count > 0){
+		while ($row = mysqli_fetch_object($get_question))
+		{
+			$id = $row->id;
+			echo "<form action='?answered=1' method='post'>
+					<p>".$row->question."</p>
+					<fieldset>";
+		}
+		if ($id != ''){
+			$get_answers = mysqli_query($db, "SELECT * FROM answers WHERE question_id=".$id." ORDER BY RAND()");
+			$answer_count = mysqli_num_rows($get_answers);
+			if ($answer_count > 0){
+				while ($row = mysqli_fetch_object($get_answers))
+				{
+					echo "<input type='radio' id='answer_a' name='Frage' value='".$row->id."'><label for='answer_a'>".$row->answer."</label>";
+				}
+			} else {
+				echo "Es konnten keine Antworten zu dieser Frage gefunden werden.";
+			}
+		} else {
+			echo "Es konnten keine Frage gefunden werden.";
+		}
+	} else {
+		echo "Es wurden bereits alle Fragen beantwortet.";
 	}
+	
 	echo "
-			
 			</fieldset>
 		<center><button>Absenden</button></center>
 	</form>";
